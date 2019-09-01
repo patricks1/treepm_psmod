4b16359c0ed00714442ab037e603115c9fe228c8
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2019-09-01 16:41:29 -0400

    messy merge with gblvar

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 
 __version__ = '0.1'
 
-setup(name = 'wTreepm',
+setup(name = 'wTreepm3',
       version = __version__,
-      description = 'Hacked, light-weight, Python3 version of Andrew Wetzels TreePM code. Originally hacked by ChangHoon Hahn. Further modified by Patrick Staudt.',
-      author='Patrick Staudt',
-      author_email='patrickstaudt1@gmail.com',
+      description = 'Hacked, light-weight, Python3 version of Andrew Wetzels TreePM code. Originally hacked by ChangHoon Hahn. Converted to Python 3 by Patrick Staudt.',
+      author='ChangHoon Hahn and Patrick Staudt',
+      author_email='hahn.changhoon@gmail.com, patrickstaudt1@gmail.com',
       url='',
       platforms=['*nix'],
       license='GPL',
       requires = ['numpy', 'scipy'],
-      provides = ['treepm_psmod'],
-      packages = ['treepm_psmod', 'treepm_psmod.utility']
+      provides = ['treepm3'],
+      packages = ['treepm3', 'treepm3.utility']
       )
